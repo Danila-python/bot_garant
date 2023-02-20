61eacd1b17ccbc7f888bd868e10856fdce65b36f
Danila-python <115091540+Danila-python@users.noreply.github.com>
2023-02-21 02:08:26 +0300

    DB#1.1: create models.py and create base.py

--- data/db/base.py
+++ data/db/base.py
@@ -1,3 +1,24 @@
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
+from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm import declarative_base
 
+SQLALCHEMY_DATABASE_URL = "postgres://localhost/users_data_base"
+
+engine = create_async_engine(
+    SQLALCHEMY_DATABASE_URL, echo=True
+)
+
 Base = declarative_base()
+
+async_session = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
+
+
+async def init_models():
+    async with engine.begin as conn:
+        await conn.run_sync(Base.metadata.drop_all)
+        await conn.run_sync(Base.metadata.create_all)
+
+
+async def get_session() -> AsyncSession:
+    async with async_session() as session:
+        yield session
--- data/db/models.py
+++ data/db/models.py
@@ -1,9 +1,12 @@
-from sqlalchemy import Column, INTEGER, String
+from sqlalchemy import Column, Integer, String, Boolean
 
 from data.db.base import Base
+
 
 class Users(Base):
     __tablename__ = "users"
 
-    id = Column(INTEGER, primary_key=True, autoincrement=True)
-    user_id = Column(String)
+    id = Column(Integer, primary_key=True, index=True, autoincrement=True)
+    user_id = Column(String, Unique=True, nullable=False)
+    user_mobile_phone = Column(String, Unique=True, nullable=False)
+    active = Column(Boolean, default=True)
