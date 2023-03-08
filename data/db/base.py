6e7ece0d37732cf600c8cb0667d432234c41b59f
Danila-python <115091540+Danila-python@users.noreply.github.com>
2023-03-08 16:52:05 +0300

    DB#1.3.2: перенёс модель внутрь database.py, потому что Alembic почему-то не видит модель...

--- data/db/base.py
+++ data/db/base.py
@@ -3,6 +3,7 @@ from sqlalchemy.orm import sessionmaker, scoped_session
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.ext.asyncio import create_async_engine
+from sqlalchemy import Column, Integer, String, Boolean
 
 # SQLALCHEMY_DATABASE_URL = "sqlite:///users_data_base"
 
@@ -15,3 +16,10 @@ async_session = sessionmaker(
     engine, class_=AsyncSession, expire_on_commit=False
 )
 
+class Users(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, index=True, autoincrement=True)
+    user_id = Column(String, unique=True, nullable=False)
+    user_mobile_phone = Column(String, primary_key=True, nullable=False)
+    active = Column(Boolean, default=True)
--- data/db/models.py
+++ data/db/models.py
@@ -1,12 +1,12 @@
-from sqlalchemy import Column, Integer, String, Boolean
-
-from data.db.base import Base
-
-
-class Users(Base):
-    __tablename__ = "users"
-
-    id = Column(Integer, index=True, autoincrement=True)
-    user_id = Column(String, unique=True, nullable=False)
-    user_mobile_phone = Column(String, primary_key=True, nullable=False)
-    active = Column(Boolean, default=True)
+# from sqlalchemy import Column, Integer, String, Boolean
+#
+# from data.db.base import Base
+#
+#
+# class Users(Base):
+#     __tablename__ = "users"
+#
+#     id = Column(Integer, index=True, autoincrement=True)
+#     user_id = Column(String, unique=True, nullable=False)
+#     user_mobile_phone = Column(String, primary_key=True, nullable=False)
+#     active = Column(Boolean, default=True)
